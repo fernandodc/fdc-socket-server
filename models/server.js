66681427cfbf3bc9722432d94f3760d44b6b3b5c
fernandodc <fernandodecaminos@gmail.com>
2020-12-09 11:33:46 -0300

    update CORS

--- models/server.js
+++ models/server.js
@@ -3,6 +3,8 @@ const http = require("http");
 const path = require("path");
 const socketio = require("socket.io");
 const Socket = require("./sockets");
+const cors = require("cors");
+
 class Server {
     constructor() {
         this.app = express();
@@ -13,6 +15,7 @@ class Server {
 
     middlewares() {
         this.app.use("/", express.static(path.resolve(__dirname, "../public")));
+        this.app.use(cors());
     }
     configurarSockets() {
         new Socket(this.io);
